3ff7fc08f081a61d44083fcec6f90a1c984c9904
SamCefalo <samuel.cefalo@gmail.com>
2022-09-28 17:47:09 -0300

    AbstractFactory Pattern

--- src/main/java/me/samcefalo/patterns/criacional/abstractFactory/models/certificate/BrazilianCertificate.java
+++ src/main/java/me/samcefalo/patterns/criacional/abstractFactory/models/certificate/BrazilianCertificate.java
@@ -2,8 +2,8 @@ package me.samcefalo.patterns.criacional.abstractFactory.models.certificate;
 
 public class BrazilianCertificate implements Certificate {
 
+	@Override
 	public String applyCertification() {
 		return "\t- Calibrating Brasilian rules\n\t- Applying Anatel's Stamp";
 	}
-
 }
--- src/main/java/me/samcefalo/patterns/criacional/abstractFactory/models/certificate/Certificate.java
+++ src/main/java/me/samcefalo/patterns/criacional/abstractFactory/models/certificate/Certificate.java
@@ -1,5 +1,6 @@
 package me.samcefalo.patterns.criacional.abstractFactory.models.certificate;
 
 public interface Certificate {
+
     String applyCertification();
 }
