0ed882f42fbfceb8565efd7b89fd414e071acfac
SamCefalo <samuel.cefalo@gmail.com>
2022-10-23 00:40:15 -0300

    Add Number format %d

--- src/main/java/me/samcefalo/teorico/Format.java
+++ src/main/java/me/samcefalo/teorico/Format.java
@@ -10,8 +10,10 @@ public class Format {
         format %,.2f => duas casas decimais com ponto (moedas)
         format %s => String
         format %t => Date
+        format %d => Número
          */
         double x;
+        int idade = 21;
         String nome = "Samuel";
         String sobrenome = "Céfalo";
         x = 2.3456;
@@ -19,7 +21,7 @@ public class Format {
 
         System.out.println(String.format("2 Casas decimais: %.2f", x));
         System.out.println(String.format("1 Casa decimal: %.1f", x));
-        System.out.println(String.format("Meu nome é %s %s e o número é %.0f", nome, sobrenome, x));
+        System.out.println(String.format("Meu nome é %s %s e tenho %d anos", nome, sobrenome, idade));
         System.out.println(String.format("Horário: %tT", data));
         System.out.println(String.format("Data: %tD", data));
         System.out.println(String.format("Mês: %tB", data));
